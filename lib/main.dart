18002df51965b4e33627763d7afa01d2a33b70f4
Michal Lysák <michelmichy7@gmail.com>
2025-10-22 22:55:30 +0200

    importing sensitive .env in app

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ import 'package:movly/features/auth/presentation/pages/liking_titles.dart';
 import 'package:movly/firebase_options.dart';
 import 'package:movly/features/auth/presentation/pages/login_page.dart';
 import 'themes/light_mode.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
