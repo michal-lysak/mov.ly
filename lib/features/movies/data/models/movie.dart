7ffc5a6c7bc1cb9b6a5fb9e798ec8831e2d9dd7f
Michal Lysák <michelmichy7@gmail.com>
2025-10-22 22:56:03 +0200

    .

---
+++ lib/features/movies/data/models/movie.dart
@@ -0,0 +1,34 @@
+class Movie {
+  final int id;
+  final String title;
+  final String overview;
+  final String posterPath;
+  final String releaseDate;
+  final double voteAverage;
+
+  Movie({
+    required this.id,
+    required this.title,
+    required this.overview,
+    required this.posterPath,
+    required this.releaseDate,
+    required this.voteAverage,
+  });
+
+  // Factory constructor: create movie into a Movie from a JSON map
+  factory Movie.fromJson(Map<String, dynamic> json) {
+    return Movie(
+      id: json['id'] as int,
+      title: (json['title'] ?? 'Untitled'),
+      overview: (json['overview'] ?? '') as String,
+      posterPath: (json['path'] ?? '') as String,
+      releaseDate: (json['release_date'] ?? 'Unknown') as String,
+      voteAverage: (json['vote_average'] ?? 0).toDouble(),
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Movie(id: $id, title: $title, voteAverage: $voteAverage)';
+  }
+}
