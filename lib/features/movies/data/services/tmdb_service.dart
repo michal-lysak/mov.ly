fa29056191ded5f28d42a4a4c87e3ad1d2651ad2
Michal Lysák <michelmichy7@gmail.com>
2025-10-24 10:28:41 +0200

    infinite scrolling added

--- lib/features/movies/data/services/tmdb_service.dart
+++ lib/features/movies/data/services/tmdb_service.dart
@@ -7,9 +7,9 @@ class TMDBService {
   final String _baseUrl = 'https://api.themoviedb.org/3';
   final String? _apiKey = dotenv.env['TMDB_API_KEY'];
 
-  Future<List<Movie>> fetchPopularMovies() async {
+  Future<List<Movie>> fetchPopularMovies({int page = 1}) async {
     final response = await http.get(
-        Uri.parse('$_baseUrl/movie/popular?api_key=$_apiKey')
+      Uri.parse('$_baseUrl/movie/popular?api_key=$_apiKey&page=$page'),
     );
 
     if (response.statusCode == 200) {
@@ -17,8 +17,7 @@ class TMDBService {
       final List results = data['results'];
       return results.map((json) => Movie.fromJson(json)).toList();
     } else {
-      throw Exception('Failed to load popular movies');
+      throw Exception('Failed to load popular movies: ${response.statusCode}');
     }
-
   }
 }
