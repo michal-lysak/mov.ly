5e38f2eb57192b3171b0a1506044d202e3323723
Michael <michelmichy7@gmail.com>
2025-10-29 22:39:09 +0100

    searchMovies code

--- lib/features/movies/data/services/tmdb_service.dart
+++ lib/features/movies/data/services/tmdb_service.dart
@@ -20,4 +20,23 @@ class TMDBService {
       throw Exception('Failed to load popular movies: ${response.statusCode}');
     }
   }
+
+  Future<List<Movie>> searchMovies(String query, {int page = 1}) async {
+    if (query.isEmpty) return [];
+
+    final encodedQuery = Uri.encodeComponent(query);
+    final response = await http.get(
+      Uri.parse(
+        '$_baseUrl/search/movie?api_key=$_apiKey&query=$encodedQuery&page=$page',
+      ),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final List results = data['results'];
+      return results.map((json) => Movie.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to search movies: ${response.statusCode}');
+    }
+  }
 }
