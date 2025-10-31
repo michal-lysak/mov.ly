c77da4819c53779d4bd734b7b4a152da74897987
Michael <michelmichy7@gmail.com>
2025-10-31 22:45:55 +0100

    implemented new custom navbar

---
+++ lib/features/movies/presentation/widgets/navbar_btn.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class NavIcon extends StatelessWidget {
+  final String iconLine;
+  final String iconSolid;
+  final bool selected;
+  final VoidCallback onTap;
+
+  const NavIcon({
+    super.key,
+    required this.iconLine,
+    required this.iconSolid,
+    required this.selected,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final icon = selected ? iconSolid : iconLine;
+
+    return GestureDetector(
+      onTap: onTap,
+      child: AnimatedScale(
+        scale: selected ? 1.15 : 1.0,
+        duration: const Duration(milliseconds: 160),
+        child: SvgPicture.asset(
+          icon,
+          width: 26,
+          colorFilter: ColorFilter.mode(
+            selected
+                ? Colors.black
+                : Colors.black.withOpacity(0.45),
+            BlendMode.srcIn,
+          ),
+        ),
+      ),
+    );
+  }
+}
