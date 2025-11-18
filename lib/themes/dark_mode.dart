b5af97f74d6b6bbd9d785706e594fcd130053303
Michael <michelmichy7@gmail.com>
2025-11-18 17:28:20 +0100

    changed color shades

--- lib/themes/dark_mode.dart
+++ lib/themes/dark_mode.dart
@@ -2,10 +2,9 @@ import 'package:flutter/material.dart';
 
 ThemeData darkMode = ThemeData(
   colorScheme: ColorScheme.dark(
-    primary: Colors.grey.shade300,       // unchanged
-    onPrimary: Colors.indigo.shade600,
-    secondary: Colors.grey.shade100,
-    tertiary: Colors.white,
+    primary: Colors.grey.shade300,
+    secondary: Colors.grey.shade900,
+    tertiary: Colors.grey.shade700,
     inversePrimary: Colors.grey.shade600, // darker reverse tone
     surface: Color(0xFF17171B),
   ),
