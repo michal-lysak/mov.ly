02dd597cc1ade22f8fcd5f2f3b66788bb74239c6
Michael <michelmichy7@gmail.com>
2025-11-21 22:00:13 +0100

    deleted trending movies

--- lib/features/movies/data/services/tmdb_service.dart
+++ lib/features/movies/data/services/tmdb_service.dart
@@ -7,20 +7,6 @@ class TMDBService {
   final String _baseUrl = 'https://api.themoviedb.org/3';
   final String? _apiKey = dotenv.env['TMDB_API_KEY'];
 
-  Future<List<Movie>> fetchTrendingMovies({String window = 'day', int page = 1}) async {
-    // window can be 'day' or 'week'
-    final response = await http.get(
-      Uri.parse('$_baseUrl/trending/movie/$window?api_key=$_apiKey&page=$page'),
-    );
-
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      final List results = data['results'];
-      return results.map((json) => Movie.fromJson(json)).toList();
-    } else {
-      throw Exception('Failed to load trending movies: ${response.statusCode}');
-    }
-  }
 
   Future<List<Movie>> fetchPopularMovies({int page = 1}) async {
     final response = await http.get(
@@ -84,7 +70,7 @@ class TMDBService {
     return keywords;
   }
 
-  Future<Movie?> fetchMovieById(String movieId) async {
+  Future<Movie?> fetchMovieById(int movieId) async {
     final url = Uri.parse('$_baseUrl/movie/$movieId?api_key=$_apiKey');
     final response = await http.get(url);
 
