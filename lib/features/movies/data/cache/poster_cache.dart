7314d46525ad93b37eb37a943cff80f337ba7573
Michael <michelmichy7@gmail.com>
2025-11-24 17:59:46 +0100

    added shimmer loading

--- lib/features/movies/data/cache/poster_cache.dart
+++ lib/features/movies/data/cache/poster_cache.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:movly/features/movies/data/models/movie.dart';
+import 'package:shimmer/shimmer.dart';
 
 class CachedPosterImage extends StatelessWidget {
   final String imageUrl;
@@ -26,12 +27,17 @@ class CachedPosterImage extends StatelessWidget {
       child: CachedNetworkImage(
         imageUrl: imageUrl,
         fit: fit,
-        placeholder: (context, url) => Container(
-          color: Colors.grey[900],
-          child: const Center(
-            child: CircularProgressIndicator(strokeWidth: 2),
+        placeholder: (context, url) => Shimmer.fromColors(
+          baseColor: Colors.grey.shade700,
+          highlightColor: Colors.grey.shade500,
+          child: Container(
+            width: 130,  // fixed poster width
+            height: 200, // match carousel height
+            color: Colors.white,
           ),
         ),
+
+
         errorWidget: (context, url, error) => Container(
           color: Colors.grey[800],
           child: const Icon(Icons.movie, color: Colors.grey, size: 48),
