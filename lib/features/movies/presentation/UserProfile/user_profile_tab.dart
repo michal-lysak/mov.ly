7bc6bb3cabdaa0d5b21f933e449e48ac5ae1859e
Michael <michelmichy7@gmail.com>
2025-12-02 21:36:03 +0100

    added bckgColor

--- lib/features/movies/presentation/UserProfile/user_profile_tab.dart
+++ lib/features/movies/presentation/UserProfile/user_profile_tab.dart
@@ -78,6 +78,7 @@ class _UserProfileTabState extends State<UserProfileTab> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: AppBar(
         title: Text(_profile?['username'] ?? "Profile"),
       ),
