876b3d82c876f1adb89dd5df13d8a26c02cef88c
Michal Lysák <michelmichy7@gmail.com>
2025-12-17 17:31:30 +0100

    fixed paths

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
-import 'package:movly/features/auth/data/auth_gate.dart'; // <-- Import the AuthGate
 import 'package:movly/features/auth/presentation/pages/signup_username_page.dart';
 import 'package:movly/features/movies/presentation/liking_titles.dart'; // Assuming this is PreHomePage
 import 'package:movly/features/movies/presentation/home/home_page.dart';
 import 'package:movly/firebase_options.dart';
-import 'features/auth/data/auth_gate.dart';
+import 'features/auth/presentation/components/auth_gate.dart';
 import 'features/movies/data/models/movie.dart';
 import 'features/movies/data/models/production_company.dart';
 import 'themes/dark_mode.dart';
